ecdcab9bdab886101f9482c631b2eff285778e1b
jussitus <juzzitus@protonmail.com>
2024-03-09 02:58:33 +0200

    ex 1.2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -8,9 +8,20 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>
-      {props.part} {props.exercises}
+      {props.name} {props.exercises}
     </p>
+
   )
 }
 
@@ -35,9 +46,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[{ name: part1, exercises: exercises1 }, { name: part2, exercises: exercises2 }, { name: part3, exercises: exercises3 }]} />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
