f7c8eed4f7d279798b40d5102b6a16e57c2c80cb
jussitus <juzzitus@protonmail.com>
2024-05-19 01:00:51 +0300

    ex 4.3

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -14,21 +14,19 @@ mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
 
 morgan.token('content', ((req) => { return req.method === 'POST' ? JSON.stringify(req.body) : '' }))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-
-app.use('/api/blogs', blogsRouter)
-
+app.use('/', blogsRouter)
 //app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
